31a6075d481f96937f004d0a450405c3f1c1943b
Lv Hung <lvhung@Lvs-MacBook-Air.local>
2025-01-13 16:16:08 +0800

    course finished.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,7 @@
+import { Metadata } from "next";
 import SideNav from "../ui/dashboard/sidenav";
+
+// export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
